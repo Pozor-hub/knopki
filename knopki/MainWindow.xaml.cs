eb509927fa229109256dd0ae2204d4a3582dadf1
student <student@K21-05-STUDENT>
2022-09-08 09:10:15 +0500

    Добавьте файлы проекта.

---
+++ knopki/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace knopki
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox == null) return;
+
+            ComboBoxItem cbi = comboBox.SelectedItem as ComboBoxItem;
+            if (cbi == null) return;
+
+            
+            FontFamily =new FontFamily(cbi.Content as string);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Slider slider = sender as Slider;
+            if (slider == null) return; 
+
+            FontSize = slider.Value;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton == null) return;
+
+            int aling = 0;
+            if (int.TryParse((string)radioButton.DataContext, out aling))
+            {
+                textBlock.TextAlignment = (TextAlignment)aling;
+            }
+        }
+    }
+}
